7f88307b19c53e30a4fa8868e77caa4547575a51
N.Y <88596711+NYOFFICIALGH@users.noreply.github.com>
2022-12-14 06:30:45 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# **Ge'ez matrix**
+![image](image/banner.jpg) 
+### <div align = "center"> **Ge'ez Digital Rain in Python in (Pygame)** </div>
+#
+## **Installation** 
+```
+apt install python
+apt install git 
+pip install pygame
+```
+#
+## **Usage**
+```
+git clone https://github.com/NYOFFICIALGH/geezmatrix
+cd geezmatrix
+python main.py
+``` 
+#
+## **Donate and feedback** 
+
+contact me : [![image](https://img.icons8.com/color/20/null/telegram-app--v1.png)](https://t.me/ny_off_tm) [Telegram](https://t.me/ny_off_tm) <br>
+        [![image](https://img.icons8.com/fluency/20/000000/instagram-new.png)](https://www.instagram.com/ny.off.ig/) [Instagram](https://www.instagram.com/ny.off.ig/) 
---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame as pg
+from random import choice,randrange
+class symbol:
+    def __init__(self,x,y,speed):
+        self.x = x
+        self.y = y
+        self.value = choice(green_char)
+        self.interval = randrange(3,10)
+        self.speed  = speed
+    def draw(self,color):
+        frames = pg.time.get_ticks()
+        if not frames % self.interval:
+            self.value = choice(green_char if color == "green" else light_green_char)
+        self.y = self.y + self.speed if self.y < height else -font_size
+        screen.blit(self.value,(self.x,self.y))
+
+class SymbolColomn:
+    def __init__(self,x,y):
+        self.colomn_height = randrange(10,20)
+        self.speed = randrange(5, 10)
+        self.symbols = [symbol(x,i,self.speed) for i in range(y,y-font_size*self.colomn_height,-font_size) ]
+    def draw(self):
+        [symbol.draw("green") if i else symbol.draw("lightgreen") for i,symbol in enumerate(self.symbols)]     
+
+pg.init()
+width = 1300
+height = 700
+font_size =  23
+blur =50
+pg.display.set_caption("Ge'ez Matrix")
+screen = pg.display.set_mode((width,height))
+surface = pg.Surface((width,height))
+
+clock = pg.time.Clock()
+
+characters = [ch for ch in "፩፪፫፬፭፮፯፰፱፲"]
+font = pg.font.SysFont(name="Power Geez Unicode1",size=font_size)
+green_char = [font.render(char,True,pg.Color(0, 255, 70)) for char in characters]
+light_green_char = [font.render(char,True,pg.Color(choice(((180, 255, 180),(0, 255, 70))))) for char in characters]
+symbolcoloms = [SymbolColomn(x,randrange(-height,0)) for x in range(0,width,font_size-10)]
+
+while True:
+    screen.blit(surface,(width,height))
+    screen.fill(pg.Color("black"))
+    [symbolcolomn.draw() for symbolcolomn in symbolcoloms   ]
+    [exit() for i in pg.event.get() if i.type == pg.QUIT]
+    pg.display.flip()
+    clock.tick(60)
